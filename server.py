2e5d946fd59f6d734c95efea76cd5cb5b185020e
ChristianMLand <christianmland@outlook.com>
2022-05-08 23:42:51 -0700

    removed db_creation step

--- server.py
+++ server.py
@@ -2,6 +2,4 @@ from flask_app import app
 from flask_app.controllers import availability_controller, user_controller, api_controller
 
 if __name__ == "__main__":
-    from flask_app.config.mysqlconnection import create_db
-    create_db()
     app.run(debug=True)
